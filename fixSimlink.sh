d9ffe58143b8d04d640fca524a1a80dbbe992857
lubomyr <lubomyr31@gmail.com>
2017-02-25 22:08:15 +0200

    fixedSymlink, joined sdlApp, updated gitignore files

--- fixSimlink.sh
+++ fixSimlink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ln -s ../../../src sdlApp/src
-ln -s ../../../res sdlApp/res
-ln -s ../../../libs app/src/main/jniLibs
+ln -s ../../../../src sdlApp/src
+ln -s ../../../../res sdlApp/res
+ln -s ../../../../libs app/src/main/jniLibs
